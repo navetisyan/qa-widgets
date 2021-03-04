8a95515d6cdbd6ee40e3c20a1b819d18865c4061
Narine Avetisyan <avetisyannarine@gmail.com>
2021-03-05 00:03:28 +0400

    accordion

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Accordion = () => {
-  return <div>Accordion</div>;
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onHandleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
+
+    return (
+      <React.Fragment key={item.title}>
+        <div
+          className={`title ${active}`}
+          onClick={() => {
+            onHandleClick(index);
+          }}
+        >
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+    </div>
+  );
 };
 
 export default Accordion;
